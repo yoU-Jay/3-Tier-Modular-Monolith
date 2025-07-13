df07beac35a096aa210c58b04ae72679313b91e7
Sohan S Uppoor <sohan88.s.u@gmail.com>
2025-07-13 18:34:07 +0530

    VPC addition

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project_name = "modular-monolith-app"
+aws_region   = "us-east-1"
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type    = string
+  default = "modular-monolith-app"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
